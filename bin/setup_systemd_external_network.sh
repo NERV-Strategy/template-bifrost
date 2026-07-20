9c68e4ffaf41c2720c58a775313f4063aaadde9c
Rijnhard Hessel <r.hessel.git@gmail.com>
2026-07-20 16:26:19 +0200

    fix: swap back to external network for external access

--- bin/setup_systemd_external_network.sh
+++ bin/setup_systemd_external_network.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+docker network create \
+  --driver bridge \
+  -o "com.docker.network.bridge.name"="bifrost-bridge" \
+  bifrost-global
+
 if ! grep -qxF "net.ipv4.conf.bifrost-bridge.route_localnet=1" /etc/sysctl.d/99-docker-bifrost.conf 2>/dev/null; then
   echo "net.ipv4.conf.bifrost-bridge.route_localnet=1" | sudo tee /etc/sysctl.d/99-docker-bifrost.conf
 fi
