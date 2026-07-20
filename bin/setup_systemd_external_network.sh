d351452ad2c8685700803a9e7e0b052d3f8f5378
Rijnhard Hessel <r.hessel.git@gmail.com>
2026-07-20 10:33:26 +0200

    fix: completely redo host networking to make it easier & more reliable.

--- bin/setup_systemd_external_network.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker network create \
-  --driver bridge \
-  -o "com.docker.network.bridge.name"="bifrost-bridge" \
-  bifrost-global
-
-echo "net.ipv4.conf.bifrost-bridge.route_localnet=1" | sudo tee /etc/sysctl.d/99-docker-bifrost.conf
-sudo sysctl -w net.ipv4.conf.bifrost-bridge.route_localnet=1
